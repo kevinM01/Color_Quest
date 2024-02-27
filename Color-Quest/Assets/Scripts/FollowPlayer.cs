d298a9bda90edf6576cafb758238bf99f332a75e
kevinM01 <masalia@usc.edu>
2024-02-26 22:37:40 -0800

    Adding New Scene, Camera angle change

--- Color-Quest/Assets/Scripts/FollowPlayer.cs
+++ Color-Quest/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
- private Vector3 offset= new Vector3(5,-1,-48);
+ private Vector3 offset= new Vector3(5,1,-48);
     public GameObject player;
     void Start()
     {
