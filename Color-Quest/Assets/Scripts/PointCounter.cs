9aad6a0dc6a655ba984921b33a0fd9bd46d8344f
Prathamesh <dhawale@usc.edu>
2024-03-04 18:06:52 -0800

    Added info when player collects coins

--- Color-Quest/Assets/Scripts/PointCounter.cs
+++ Color-Quest/Assets/Scripts/PointCounter.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class PointCounter : MonoBehaviour
 {
     public TextMeshProUGUI pointsText;
     public int points;
     private PlayerJump playerJump;
+    public TextMeshProUGUI firstCoinText;
+    private bool flag;
 
     void Start()
     {
         playerJump = FindObjectOfType<PlayerJump>();
         points = 0;
         UpdatePointsText();
+        firstCoinText.enabled = false;
+        flag = false;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -22,6 +27,12 @@ public class PointCounter : MonoBehaviour
             points++;
             UpdatePointsText();
 
+            if(points == 1 && !flag)
+            {
+                StartCoroutine(ShowFirstCoinText());
+                flag = true;
+            }
+
             // Check if the player has collected 5 points
             if (points >= 5)
             {
@@ -29,6 +40,23 @@ public class PointCounter : MonoBehaviour
                 playerJump.IncreaseHealthByAmount(10);
             }
         }
+
+//
+//        if (other.CompareTag("firstCoin"))
+//                {
+//                    // Increment points when the player collects coins
+//                    points++;
+//                    UpdatePointsText();
+//
+//
+//
+//                    // Check if the player has collected 5 points
+//                    if (points >= 5)
+//                    {
+//                        // Invoke a method to increase the player's health by 10
+//                        playerJump.IncreaseHealthByAmount(10);
+//                    }
+//                }
     }
 
     public void UpdatePointsText()
@@ -36,4 +64,11 @@ public class PointCounter : MonoBehaviour
         // Update the points text UI element
         pointsText.text = "Points: " + points.ToString();
     }
+
+    IEnumerator ShowFirstCoinText()
+        {
+            firstCoinText.enabled = true;
+            yield return new WaitForSeconds(4);
+            firstCoinText.enabled = false;
+        }
 }
