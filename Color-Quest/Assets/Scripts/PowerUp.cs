a95ca614f52a3821e178c281cdde98c9f74731e5
Atharva Dixit <48327037+atharvadixit@users.noreply.github.com>
2024-03-25 23:06:17 -0700

    Added Power Up collection Text
    Made script changes to show the meaning of Power Up collection when user collects it for the first time.

--- Color-Quest/Assets/Scripts/PowerUp.cs
+++ Color-Quest/Assets/Scripts/PowerUp.cs
@@ -1,14 +1,37 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class DestroyCollectible : MonoBehaviour
 {
+    private bool flag;
+    public TextMeshProUGUI firstPowerUpText;
+
+    void Start()
+    {
+        firstPowerUpText.enabled = false;
+        flag = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Collect(other.gameObject);
+            if(!flag)
+            {
+                StartCoroutine(ShowPowerUpText());
+                flag = false;
+            }
         }
     }
+
+    IEnumerator ShowPowerUpText()
+        {
+            firstPowerUpText.enabled = true;
+            yield return new WaitForSeconds(4);
+            firstPowerUpText.enabled = false;
+        }
 
     private void Collect(GameObject player)
     {
