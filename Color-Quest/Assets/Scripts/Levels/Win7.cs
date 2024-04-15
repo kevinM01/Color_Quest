f56146c7befcf9eb22be8ed8e209b04c366f265a
kevinM01 <masalia@usc.edu>
2024-04-15 02:37:39 -0700

    Adding win scenes

---
+++ Color-Quest/Assets/Scripts/Levels/Win7.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win7 : MonoBehaviour
+{
+    private PlayerJump playerJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+         playerJump = FindObjectOfType<PlayerJump>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerJump.SendCoinXHealthAnalytics();
+            SceneManager.LoadScene("Win7");
+        }
+}
+}
