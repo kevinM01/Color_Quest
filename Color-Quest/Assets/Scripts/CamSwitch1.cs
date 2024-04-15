6628e0c8531d74d47951478740a77f89eaf9b87e
kevinM01 <masalia@usc.edu>
2024-04-15 01:34:10 -0700

    Fixing bugs

--- Color-Quest/Assets/Scripts/CamSwitch1.cs
+++ Color-Quest/Assets/Scripts/CamSwitch1.cs
@@ -6,18 +6,18 @@ public class CamSwitch1 : MonoBehaviour
 { 
     public GameObject maincam;
     public GameObject previecam;
-     public GameObject player; 
+    //  public GameObject player; 
     void Start()
     {
          maincam.SetActive (false);
         StartCoroutine (Preview());
-         player.SetActive (false);
+        //  player.SetActive (false);
     }
 
     // Update is called once per frame
     IEnumerator Preview(){
         yield return new WaitForSeconds (4);
-        player.SetActive (true);
+        // player.SetActive (true);
         maincam.SetActive (true);
         previecam.SetActive (false);
     }
--- Color-Quest/Assets/Scripts/CamSwitch5.cs
+++ Color-Quest/Assets/Scripts/CamSwitch5.cs
@@ -7,18 +7,18 @@ public class CamSwitch5 : MonoBehaviour
     // Start is called before the first frame update
      public GameObject maincam;
     public GameObject previecam;
-     public GameObject player; 
+    //  public GameObject player; 
     void Start()
     {
          maincam.SetActive (false);
         StartCoroutine (Preview());
-         player.SetActive (false);
+        //  player.SetActive (false);
     }
 
     // Update is called once per frame
     IEnumerator Preview(){
         yield return new WaitForSeconds (10);
-        player.SetActive (true);
+        // player.SetActive (true);
         maincam.SetActive (true);
         previecam.SetActive (false);
     }
