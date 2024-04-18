c0ad359dc2ae8501e52260721a69afd952bdbbcd
Prathamesh Dhawale <dhawale@usc.edu>
2024-04-18 00:19:19 -0700

    changed background color and added zoom-in zoom-out script

--- Color-Quest/Assets/Scripts/FollowPlayer.cs
+++ Color-Quest/Assets/Scripts/FollowPlayer.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
- private Vector3 offset= new Vector3(5,1,-48);
     public GameObject player;
+    public float zoomOutSize = 13.0f;  // Orthographic size when zoomed out
+    private float originalSize;        // To store the original size
+    public float zoomDistance = 20.0f;
+
+    private Vector3 offset = new Vector3(5, 1, -48);
+    private Vector3 originalOffset;
+    private bool isZoomedOut = false;
+
     void Start()
     {
-        
+        originalOffset = offset;
+        originalSize = GetComponent<Camera>().orthographicSize;  // Store the original orthographic size
     }
+
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ToggleZoom();
+        }
+        transform.position = player.transform.position + offset;
+    }
 
+    void ToggleZoom()
+    {
+        Camera camera = GetComponent<Camera>();
+        if (isZoomedOut)
+        {
+            // Zoom in (reset to original offset and size)
+            offset = originalOffset;
+            camera.orthographicSize = originalSize;
+            isZoomedOut = false;
+        }
+        else
+        {
+            // Zoom out (increase z offset and increase size)
+            offset = new Vector3(offset.x, offset.y, originalOffset.z - zoomDistance);
+            camera.orthographicSize = zoomOutSize;
+            isZoomedOut = true;
+        }
     }
 }
